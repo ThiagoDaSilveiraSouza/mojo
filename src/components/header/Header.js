869faeb7cb5883421002eda30b1497738d9b1d0d
Thiago da Silveira Souza <thiagodasilveirasouza@gmail.com>
2021-01-14 18:03:17 -0300

    change mouse event on header
    change "onMouseEnter" for "onMouseOver"

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -99,8 +99,8 @@ export const Header = () => {
       </header>
       <div
         className='invisible-header'
-        onMouseEnter={() => {
-          setHeaderVisibility(true)
+        onMouseOver={() => {
+          !headerVisibility && setHeaderVisibility(true)
         }}
       />
     </>
